6d71285dcee822d7fd321ccdf3e856124a438a0c
Akash Mozumdar <akashmozumdar@gmail.com>
2020-03-27 04:59:48 -0600

    break before definition

--- jmdict_to_textractor.py
+++ jmdict_to_textractor.py
@@ -42,7 +42,7 @@ for entry in parse(argv[1]).getroot().iter("entry"):
 
 	for term, definitions in definitions_by_term.items():
 		outfile.writelines(f"|TERM|{inflection}" for inflection in inflect(term))
-		outfile.write(f"|DEFINITION|<small>({', '.join(filter(lambda word: word not in exclude, term.words))})</small>")
-		outfile.writelines(f"\n<p>{definition}</p>" for definition in definitions)
+		outfile.write(f"|DEFINITION|<p><small>({', '.join(filter(lambda word: word not in exclude, term.words))})</small>")
+		outfile.writelines(f"\n<p>{definition}" for definition in definitions)
 		outfile.write("|END|\n")
 outfile.write(open("inflections.txt", "r", encoding="utf-8").read())
